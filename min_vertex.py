0ab0f4340e2f8b6a5655c2cc15b5bf5925cc0d09
Timo Witte <timo.witte@gmail.com>
2019-05-09 00:35:37 +0200

    visualize using matplotlib + random graph with 20 nodes and more connections

--- min_vertex.py
+++ min_vertex.py
@@ -1,11 +1,21 @@
+import numpy as np
 import networkx as nx
 import dwave_networkx as dnx
+import matplotlib.pyplot as plt
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
 
-mygraph = nx.star_graph(4)
+mygraph = nx.dense_gnm_random_graph(20,40)
+
+pos=nx.circular_layout(mygraph)
 
 sampler = EmbeddingComposite(DWaveSampler())
 result = dnx.min_vertex_cover(mygraph, sampler)
 
 print(result)
+
+nx.draw_networkx_nodes(mygraph,pos, with_labels=True)
+nx.draw_networkx_nodes(mygraph,pos, with_labels=True, node_color='r', nodelist=result)
+nx.draw_networkx_edges(mygraph, pos)
+
+plt.show()
